aa5f95c9999451b022cfdb299b3f378c0aea1c39
Dor Peled <knat.dev.93@gmail.com>
2023-12-11 18:17:20 +0200

    logging added

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -4,8 +4,13 @@
 
 extern Hazel::Application* Hazel::CreateApplication();
 
-int main() {
-	printf("Hazel Engine\n");
+int main(int argc, char** argv) {
+	Hazel::Log::Init();
+
+	HZ_CORE_INFO("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
