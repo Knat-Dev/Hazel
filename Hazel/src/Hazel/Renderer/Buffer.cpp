e990eb5532f7280e0231081f59c7e775b9da76b6
Dor Peled <knat.dev.93@gmail.com>
2023-12-16 16:55:09 +0200

    returning value for all branches

--- Hazel/src/Hazel/Renderer/Buffer.cpp
+++ Hazel/src/Hazel/Renderer/Buffer.cpp
@@ -18,6 +18,9 @@ namespace Hazel {
 		default:
 			break;
 		}
+
+		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
 	}
 
 	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
@@ -32,5 +35,8 @@ namespace Hazel {
 		default:
 			break;
 		}
+
+		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
 	}
 }
